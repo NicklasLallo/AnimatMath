9eb7b7a3c7fc7a3f516f0f62844fd7dc7fb15496
Nicklas Lallo <lallo@student.chalmers.se>
2018-04-30 03:24:08 +0000

    added comments.

--- GPtestar/amazon.py
+++ GPtestar/amazon.py
@@ -2,12 +2,17 @@ import json
 import re
 from pprint import pprint
 
+#Custom splitchar now that blankspace isn't as useful
 splitchar = "$"
+
+# To get the raw input datafiles, see this website: http://jmcauley.ucsd.edu/data/amazon/ 
 data = json.load(open('data.json'))
 
 
+#Output filename
 f = open("naturallanguage.dat", "w")
 
+#Manualy writing out the characters. Not optimal
 f.write("splitChar:" + splitchar + "actionList:qwertyuiopasdfghjklzxcvbnm,.1234567890\n chars:qwertyuiopasdfghjklzxcvbnm,.1234567890\n")
 #for data in dataSet:
 #    f.write(data+"\n")
@@ -16,24 +21,22 @@ f.write("splitChar:" + splitchar + "actionList:qwertyuiopasdfghjklzxcvbnm,.12345
 
 i = 1
 for entry in data:
-    row = entry["reviewText"]
-    row = re.sub(r'[^a-zA-Z0-9., ]', '', row)
-    row = str(row)
-    row = re.split(" ", row)
+    row = entry["reviewText"] #Ignore all other fields from the raw input data
+    row = re.sub(r'[^a-zA-Z0-9., ]', '', row) #Remove all special characters with regular expressions
+    row = str(row) #Probably doesn't do anything, can be removed?
+    row = re.split(" ", row) #Make it from a single long string into an list of words
     #print("{}\n".format(len(row)))
-    row2 = row[-8:]
-    row = row[:-8]
+    row2 = row[-8:] #Make the 8 last words into the output
+    row = row[:-8] #Make all except the 8 last words into the input
     for word in row:
-        f.write(word + " ")
+        f.write(word + " ") 
     f.write(splitchar)
     for word in row2:
         f.write(word + " ")
     f.write("\n")
     i += 1
 
-    if i == 100000:
+    if i == 100000: #Limit the number of lines to be processed in order to save time and space currently
         break
-    #print("{}\n".format(len(row)))
 
 f.close()
-#print(len(data))
